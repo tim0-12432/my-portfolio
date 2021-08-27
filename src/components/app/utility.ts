5f3c8e352fbb45312f03a6985b76ee0949264c48
tim0-12432 <79634593+tim0-12432@users.noreply.github.com>
2021-08-27 16:10:08 +0200

    fix undersizing

--- src/components/app/utility.ts
+++ src/components/app/utility.ts
@@ -1,6 +1,5 @@
 export function isMobileRatio() {
 	const ratio = document.body.clientHeight / document.body.clientWidth;
-	console.log(document.body.clientWidth, ratio);
 	if (ratio > 20/4) {
 		return true;
 	}
