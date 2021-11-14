3ab691c735272b88c57e7528e62c5a1a31081837
tim0-12432 <79634593+tim0-12432@users.noreply.github.com>
2021-11-14 21:10:11 +0100

    fix ssr window clientsizes

--- pages/components/app/utility.ts
+++ pages/components/app/utility.ts
@@ -1,10 +1,13 @@
-export function isMobileRatio() {
-	const ratio = document.body.clientHeight / document.body.clientWidth;
-	if (ratio > 20/4) {
-		return true;
+export function isMobileRatio(): boolean {
+	let result = false;
+	if (typeof window !== "undefined") {
+		const ratio = document.body.clientHeight / document.body.clientWidth;
+		if (ratio > 20/4) {
+			result = true;
+		}
+		if (document.body.clientWidth < 700) {
+			result = true;
+		}
 	}
-	if (document.body.clientWidth < 700) {
-		return true;
-	}
-	return false;
+	return result;
 }
