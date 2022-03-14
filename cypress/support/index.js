7e77eb4334d7c3a6654326d6f52a27759b49d927
tim0-12432 <79634593+tim0-12432@users.noreply.github.com>
2022-03-14 22:03:19 +0100

    next init

--- cypress/support/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-import "./commands";
-import "cypress-react-unit-test/support";
-
-Cypress.on("uncaught:exception", (err) => {
-	console.log(err);
-	return false;
-});
--- src/resources/constants/Mixins.ts
+++
@@ -1,6 +0,0 @@
-
-export const alignDisplayCenter = `
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
